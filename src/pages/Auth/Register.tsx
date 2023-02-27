bbd8e009cdeb3848454d0794fc84f302f4ae80de
Mateus Gurgel <mateusgurgel15@gmail.com>
2023-02-27 14:05:32 -0300

    handleSubmit added

--- src/pages/Auth/Register.tsx
+++ src/pages/Auth/Register.tsx
@@ -1,35 +1,71 @@
-import "./Auth.css"
+import "./Auth.css";
 
+import { FormEvent, useState } from "react";
 import { Link } from "react-router-dom";
 
 const Register = () => {
+  const [name, setName] = useState<string>("");
+  const [email, setEmail] = useState<string>("");
+  const [password, setPassword] = useState<string>("");
+  const [confirmPassword, setConfirmPassword] = useState<string>("");
+
+  const handleSubmit = (e: FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+
+    const user = {
+      name,
+      email,
+      password,
+      confirmPassword,
+    };
+
+    console.log(user);
+  };
+
   return (
     <div id="register">
       <h2>ReactGram</h2>
       <p className="subtitle">Cadastre-se para ver as fotos dos seus amigos.</p>
-      <form>
+      <form onSubmit={handleSubmit}>
         <input
           type="text"
           placeholder="Nome"
+          value={name}
+          onChange={(e) => {
+            setName(e.target.value);
+          }}
         />
         <input
           type="email"
           placeholder="E-mail"
+          value={email}
+          onChange={(e) => {
+            setEmail(e.target.value);
+          }}
         />
         <input
           type="password"
           placeholder="Senha"
+          value={password}
+          onChange={(e) => {
+            setPassword(e.target.value);
+          }}
         />
         <input
           type="password"
+          value={confirmPassword}
           placeholder="Confirme a senha"
+          onChange={(e) => {
+            setConfirmPassword(e.target.value);
+          }}
         />
+        <input type="submit" value="Register" />
       </form>
       <p>
         Já tem conta? <Link to="/login">Clique aqui</Link>
       </p>
     </div>
-  )
-}
+  );
+};
 
-export default Register
+export default Register;
