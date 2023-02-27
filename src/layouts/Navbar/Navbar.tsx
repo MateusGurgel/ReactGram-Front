aa92dfa50077e66e35ed991903414c38416ff189
Mateus Gurgel <mateusgurgel15@gmail.com>
2023-02-27 13:41:40 -0300

    Links added

--- src/layouts/Navbar/Navbar.tsx
+++ src/layouts/Navbar/Navbar.tsx
@@ -17,7 +17,13 @@ const Navbar = () => {
         <input type="text" />
       </form>
 
-
+      <ul id="nav-links">
+        <NavLink to="/">
+          <BsHouseDoorFill />
+        </NavLink>
+        <NavLink to="/login">Login</NavLink>
+        <NavLink to="/register">Register</NavLink>
+      </ul>
     </nav>
   );
 };
