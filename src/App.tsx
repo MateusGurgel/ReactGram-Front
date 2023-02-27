fa5568aca1956e234473dfed695c14d88a995925
Mateus Gurgel <mateusgurgel15@gmail.com>
2023-02-27 13:23:56 -0300

    Navbar added

--- src/App.tsx
+++ src/App.tsx
@@ -4,15 +4,19 @@ import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
 import Home from "./pages/Home/Home";
 import Login from "./pages/Auth/Login";
 import Register from "./pages/Auth/Register";
+import Navbar from "./layouts/Navbar/Navbar";
+import Footer from "./layouts/Footer/Footer";
 
 function App() {
   return (
     <BrowserRouter>
+      <Navbar/>
       <Routes>
         <Route path="/" element={<Home/>}/>
         <Route path="/login" element={<Login/>}/>
         <Route path="/register" element={<Register/>}/>
       </Routes>
+      
     </BrowserRouter>
   );
 }
