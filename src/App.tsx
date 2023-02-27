a04970d24b14e8369ecfa4afe1ca649a12fac2e8
Mateus Gurgel <mateusgurgel15@gmail.com>
2023-02-27 13:08:02 -0300

    register route added

--- src/App.tsx
+++ src/App.tsx
@@ -11,6 +11,7 @@ function App() {
       <Routes>
         <Route path="/" element={<Home/>}/>
         <Route path="/login" element={<Login/>}/>
+        <Route path="/register" element={<Register/>}/>
       </Routes>
     </BrowserRouter>
   );
