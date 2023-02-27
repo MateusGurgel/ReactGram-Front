7f7e66f69afed5753cace12b71150a3f460908fb
Mateus Gurgel <mateusgurgel15@gmail.com>
2023-02-27 20:38:18 -0300

    form added

--- src/pages/Auth/Login.tsx
+++ src/pages/Auth/Login.tsx
@@ -1,7 +1,58 @@
+import { FormEvent, useState } from "react";
+import { Link } from "react-router-dom";
 import "./Auth.css";
 
 const Login = () => {
-  return <div>Login</div>;
+  const [password, setPassword] = useState<string>("");
+  const [email, setEmail] = useState<string>("");
+  const [name, setName] = useState<string>("");
+
+  const handleSubmit = (e: FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+
+    const user = {
+      name,
+      email,
+      password,
+    };
+
+    console.log(user);
+  };
+
+  return (
+    <div id="login">
+      <h2>ReactGram</h2>
+      <p className="subtitle">log in to see your friend's photos.</p>
+      <form onSubmit={handleSubmit}>
+        <input
+          type="text"
+          placeholder="Name"
+          onChange={(e) => {
+            setName(e.target.value);
+          }}
+        />
+        <input
+          type="email"
+          placeholder="Email"
+          onChange={(e) => {
+            setEmail(e.target.value);
+          }}
+        />
+        <input
+          type="password"
+          placeholder="Name"
+          onChange={(e) => {
+            setPassword(e.target.value);
+          }}
+        />
+
+        <input type="submit" value="Login" />
+      </form>
+      <p>
+        Don't have an account? <Link to="/login">Sign up </Link>
+      </p>
+    </div>
+  );
 };
 
 export default Login;
