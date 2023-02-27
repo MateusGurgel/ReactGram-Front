9d56d69e589a09e0bca8ed9c1e3fd4474fd25ae8
Mateus Gurgel <mateusgurgel15@gmail.com>
2023-02-27 13:24:18 -0300

    Footer added

--- src/App.tsx
+++ src/App.tsx
@@ -16,7 +16,7 @@ function App() {
         <Route path="/login" element={<Login/>}/>
         <Route path="/register" element={<Register/>}/>
       </Routes>
-      
+      <Footer/>
     </BrowserRouter>
   );
 }
