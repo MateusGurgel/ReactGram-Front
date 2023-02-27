0f0fd2dbf42e56574235e7e63e621bb34de3b0b9
Mateus Gurgel <mateusgurgel15@gmail.com>
2023-02-27 13:18:37 -0300

    Form added

--- src/pages/Auth/Register.tsx
+++ src/pages/Auth/Register.tsx
@@ -1,8 +1,34 @@
 import "./Auth.css"
 
+import { Link } from "react-router-dom";
+
 const Register = () => {
   return (
-    <div>Register</div>
+    <div id="register">
+      <h2>ReactGram</h2>
+      <p className="subtitle">Cadastre-se para ver as fotos dos seus amigos.</p>
+      <form>
+        <input
+          type="text"
+          placeholder="Nome"
+        />
+        <input
+          type="email"
+          placeholder="E-mail"
+        />
+        <input
+          type="password"
+          placeholder="Senha"
+        />
+        <input
+          type="password"
+          placeholder="Confirme a senha"
+        />
+      </form>
+      <p>
+        Já tem conta? <Link to="/login">Clique aqui</Link>
+      </p>
+    </div>
   )
 }
 
