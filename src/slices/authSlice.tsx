cfbd89268e004a662d582d5b7e705093e71cf5a7
Mateus <mateusgurgel15@gmail.com>
2023-02-28 12:00:22 -0300

    initial commit

---
+++ src/slices/authSlice.tsx
@@ -0,0 +1,61 @@
+import { createSlice, createAsyncThunk } from "@reduxjs/toolkit";
+import authService from "../services/authService";
+
+const user = JSON.parse(localStorage.getItem("user") || "null");
+
+const initialState = {
+  user: user != "null" ? user : null,
+  error: false,
+  success: false,
+  loading: false,
+  status: "",
+};
+
+export const register = createAsyncThunk(
+  "auth/register",
+  async (user: any, thunkAPI) => {
+    const data = await authService.register(user);
+
+    if (data.errors) {
+      return thunkAPI.rejectWithValue(data.errors[0]);
+    }
+
+    return data;
+  }
+);
+
+export const authSlice = createSlice({
+  name: "auth",
+  initialState,
+  reducers: {
+    reset: (state) => {
+      state.loading = false;
+      state.error = false;
+      state.success = false;
+    },
+  },
+  extraReducers: (builder) => {
+    builder
+
+      .addCase(register.pending, (state) => {
+        state.loading = true
+        state.error = false
+        state.success = false
+      })
+      .addCase(register.fulfilled, (state, action) => {
+        state.loading = false;
+        state.success = true;
+        state.error = false;
+        state.user = action.payload;
+      })
+      .addCase(register.rejected, (state, action) => {
+        state.loading = false;
+        state.success = false;
+        state.user = null;
+        state.status = String(action.payload);
+      });
+  },
+});
+
+export const { reset } = authSlice.actions;
+export default authSlice.reducer;
