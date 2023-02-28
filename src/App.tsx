9fd0a8374c76a9ea2eedfc1479ebd27e40f9ff37
Mateus Gurgel <mateusgurgel15@gmail.com>
2023-02-28 17:48:54 -0300

    auth added

--- src/App.tsx
+++ src/App.tsx
@@ -6,16 +6,23 @@ import Login from "./pages/Auth/Login";
 import Register from "./pages/Auth/Register";
 import Navbar from "./layouts/Navbar/Navbar";
 import Footer from "./layouts/Footer/Footer";
+import { useAuth } from "./hooks/useAuth";
 
 function App() {
+  const {auth, loading} = useAuth()
+
+  if (loading){
+    return ( <h1>Loading...</h1> )
+  }
+
   return (
     <BrowserRouter>
       <Navbar />
       <div className="container">
         <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="/login" element={<Login />} />
-          <Route path="/register" element={<Register />} />
+          <Route path="/" element={auth ? <Home /> : <Navigate to="/login"/>} />
+          <Route path="/login" element={!auth ? <Login /> : <Navigate to="/"/>} />
+          <Route path="/register" element={!auth ? <Register /> : <Navigate to="/"/>} />
         </Routes>
       </div>
       <Footer />
