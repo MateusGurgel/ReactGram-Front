bad6c3c2fb6252efc6aec23a0eaab074e1926cee
Mateus <mateusgurgel15@gmail.com>
2023-02-28 12:49:31 -0300

    formated

--- src/services/authService.tsx
+++ src/services/authService.tsx
@@ -1,29 +1,25 @@
-import {api, requestConfig} from '../utils/config'
+import { api, requestConfig } from "../utils/config";
 
-const register = async(data : any) => {
+const register = async (data: any) => {
+  const config = requestConfig("POST", data);
 
-    const config = requestConfig("POST", data)
+  try {
+    const res = await fetch(api + "/users/register", config)
+      .then((res) => res.json())
+      .catch((err) => err);
 
-    console.log(config)
-
-    try{
-        const res = await fetch(api + "/users/register", config)
-        .then((res) => res.json())
-        .catch((err) => err);
-
-        if(res) {
-            localStorage.setItem("user", JSON.stringify(res))
-        }
-
-        return res;
-    } catch (error) {
-        console.log(error)
+    if (res) {
+      localStorage.setItem("user", JSON.stringify(res));
     }
 
-}
+    return res;
+  } catch (error) {
+    console.log(error);
+  }
+};
 
 const authService = {
-    register
-}
+  register,
+};
 
-export default authService
+export default authService;
