39e97d4a3ff56f171da6c1155447a251fc5277f6
Mateus Gurgel <mateusgurgel15@gmail.com>
2023-02-27 22:07:42 -0300

    Provider added

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,15 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App'
-import './assets/index.css'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App";
+import "./assets/index.css";
 
-ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
+import { Provider } from "react-redux";
+import { store } from "./store";
+
+ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
   <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-)
+    <Provider store={store}>
+      <App />
+    </Provider>
+  </React.StrictMode>
+);
