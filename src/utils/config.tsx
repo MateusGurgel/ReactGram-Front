a7f95735782da641b50e70f830aa11d450f53cf8
Mateus <mateusgurgel15@gmail.com>
2023-02-28 11:59:24 -0300

    initial commit

---
+++ src/utils/config.tsx
@@ -0,0 +1,38 @@
+export const api = "http://localhost:5234/api";
+export const uploads = "http://localhost:5234/uploads";
+
+export const requestConfig = (
+  method: string,
+  data?: BodyInit,
+  token?: string,
+  image?: Blob
+) => {
+  let config: RequestInit;
+  method = method.toUpperCase();
+
+  if (image) {
+    config = {
+      method: method.toUpperCase(),
+      body: data,
+      headers: {},
+    };
+  } else if (method === "DELETE" || data === null) {
+    config = {
+      method: method,
+    };
+  } else {
+    config = {
+      method: method,
+      body: JSON.stringify(data),
+      headers: {
+        "Content-Type": "application/json",
+      },
+    };
+  }
+
+  if (token) {
+    config.headers = {...config.headers, "Authorization" : `Bearer ${token}`}
+  }
+
+  return config;
+};
