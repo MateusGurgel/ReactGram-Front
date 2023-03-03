d54b077c10463b9a0f3805d0ee7f651114a3b08c
Mateus Gurgel <mateusgurgel15@gmail.com>
2023-03-03 17:29:21 -0300

    getUserPhotos added

--- src/services/photoService.tsx
+++ src/services/photoService.tsx
@@ -1,19 +1,33 @@
-import {api, requestConfig} from "../utils/config"
+import { api, requestConfig } from "../utils/config";
 
 const publishPhoto = async (data: any, token: string) => {
-    const config = requestConfig("POST", data, token, true);
-  
-    try {
-      const res = await fetch(api + "/photos", config)
-        .then((res) => res.json())
-        .catch((err) => err);
-  
-      return res;
-    } catch (error) {
-      console.log(error);
-    }
-  };
+  const config = requestConfig("POST", data, token, true);
 
-const photoService = { publishPhoto }
+  try {
+    const res = await fetch(api + "/photos", config)
+      .then((res) => res.json())
+      .catch((err) => err);
 
-export default photoService
+    return res;
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+const getUserPhotos = async (id: string) => {
+  const config = requestConfig("GET");
+
+  try {
+    const res = await fetch(api + "/photos/user/" + id, config)
+      .then((res) => res.json())
+      .catch((err) => err);
+
+    return res;
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+const photoService = { publishPhoto, getUserPhotos };
+
+export default photoService;
