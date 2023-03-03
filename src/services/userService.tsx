7495bdd84c1004d83dfef7e600bf24171a4e5e14
Mateus <mateusgurgel15@gmail.com>
2023-03-03 09:07:49 -0300

    updateProfile added

--- src/services/userService.tsx
+++ src/services/userService.tsx
@@ -15,8 +15,26 @@ const profile = async (data: any, token: string) => {
     }
   };
 
+  const updateProfile = async (data: any, token: string) => {
+    const config = requestConfig("PUT", data, token, true);
+  
+    console.log(config);
+    console.log(data);
+  
+    try {
+      const res = await fetch(api + "/users/", config)
+        .then((res) => res.json())
+        .catch((err) => err);
+  
+      return res;
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
   const userService = {
     profile
   }
+
 
   export default userService;
