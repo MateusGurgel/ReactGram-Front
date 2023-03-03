b8cb205e2a96f8d6da31daf03b387cbd3c4b53aa
Mateus <mateusgurgel15@gmail.com>
2023-03-03 12:27:48 -0300

    photoReducer added

--- src/store.tsx
+++ src/store.tsx
@@ -1,11 +1,14 @@
 import { configureStore } from "@reduxjs/toolkit";
 
 import authReducer from './slices/authSlice'
+import photoReducer from "./slices/photoSlice";
 import userReducer from './slices/userSlice'
 
 export const store = configureStore({
     reducer: {
         auth: authReducer,
-        user: userReducer 
+        user: userReducer,
+        photo: photoReducer
+
     }
 })
