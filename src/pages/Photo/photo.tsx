7f2be23446b8dccdc1b3d528d99dc4dd290d2886
Mateus Gurgel <mateusgurgel15@gmail.com>
2023-03-04 17:27:06 -0300

    PhotoItem added

--- src/pages/Photo/photo.tsx
+++ src/pages/Photo/photo.tsx
@@ -1,9 +1,36 @@
-import "./Photo.css"
+import "./Photo.css";
+
+import { uploads } from "../../utils/config";
+import { useDispatch, useSelector } from "react-redux";
+import { useParams } from "react-router-dom";
+import { useState, useEffect } from "react";
+import { store } from "../../store";
+import { getPhoto } from "../../slices/photoSlice";
+import PhotoItem from "../../components/PhotoItem/PhotoItem";
 
 const Photo = () => {
-  return (
-    <div>photo</div>
-  )
-}
+  const { id } = useParams();
 
-export default Photo
+  const dispatch = useDispatch();
+
+  const { photo, loading, error, message } = useSelector((state: any) => state.photo);
+
+  useEffect(() => {
+    store.dispatch(getPhoto(id || "undefined"));
+  }, [dispatch, id]);
+
+
+  if (loading) {
+    return <p>Carregando...</p>;
+  }
+
+  console.log(photo)
+
+  return (
+    <div id="photo">
+      <PhotoItem photo={photo} />
+    </div>
+  );
+};
+
+export default Photo;
