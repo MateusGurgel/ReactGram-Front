f2a11c02fbe116dd0d7de165b13247124954cd12
Mateus Gurgel <mateusgurgel15@gmail.com>
2023-03-04 15:25:42 -0300

    photo path added

--- src/App.tsx
+++ src/App.tsx
@@ -9,6 +9,7 @@ import Footer from "./layouts/Footer/Footer";
 import { useAuth } from "./hooks/useAuth";
 import EditProfile from "./pages/EditProfile/EditProfile";
 import Profile from "./pages/Profile/Profile";
+import Photo from "./pages/Photo/photo";
 
 function App() {
   const {auth, loading} = useAuth()
@@ -27,6 +28,7 @@ function App() {
           <Route path="/profile" element={auth ? <EditProfile/> : <Navigate to="/login"/>} />
           <Route path="/login" element={!auth ? <Login /> : <Navigate to="/"/>} />
           <Route path="/register" element={!auth ? <Register /> : <Navigate to="/"/>} />
+          <Route path="/photos/:id" element={auth ? <Photo/> : <Navigate to="/login"/>}/>
         </Routes>
       </div>
       <Footer />
