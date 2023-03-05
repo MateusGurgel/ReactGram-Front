4d51054532229938fc4e24359e8542cd6daa8011
Mateus Gurgel <mateusgurgel15@gmail.com>
2023-03-05 14:27:18 -0300

    "/profile/:id" to  "/users/:id"

--- src/App.tsx
+++ src/App.tsx
@@ -24,7 +24,7 @@ function App() {
       <div className="container">
         <Routes>
           <Route path="/" element={auth ? <Home /> : <Navigate to="/login"/>} />
-          <Route path="/profile/:id" element={auth ? <Profile/> : <Navigate to="/login"/>}/>
+          <Route path="/users/:id" element={auth ? <Profile/> : <Navigate to="/login"/>}/>
           <Route path="/profile" element={auth ? <EditProfile/> : <Navigate to="/login"/>} />
           <Route path="/login" element={!auth ? <Login /> : <Navigate to="/"/>} />
           <Route path="/register" element={!auth ? <Register /> : <Navigate to="/"/>} />
