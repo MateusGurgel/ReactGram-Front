87cb2d3db2039abbaff9b35585fde74b5875e81d
Mateus Gurgel <mateusgurgel15@gmail.com>
2023-03-05 01:06:16 -0300

    like added

--- src/pages/Photo/photo.tsx
+++ src/pages/Photo/photo.tsx
@@ -5,30 +5,34 @@ import { useDispatch, useSelector } from "react-redux";
 import { useParams } from "react-router-dom";
 import { useState, useEffect } from "react";
 import { store } from "../../store";
-import { getPhoto } from "../../slices/photoSlice";
+import { getPhoto, like } from "../../slices/photoSlice";
 import PhotoItem from "../../components/PhotoItem/PhotoItem";
+import LikeContainer from "../../components/LikeContainer/LikeContainer";
 
 const Photo = () => {
   const { id } = useParams();
 
   const dispatch = useDispatch();
 
+  const { user } = useSelector((state : any) => state.auth);
   const { photo, loading, error, message } = useSelector((state: any) => state.photo);
 
   useEffect(() => {
     store.dispatch(getPhoto(id || "undefined"));
   }, [dispatch, id]);
 
+  const handleLike = () => {
+    store.dispatch(like(photo._id));
+  };
 
   if (loading) {
     return <p>Carregando...</p>;
   }
 
-  console.log(photo)
-
   return (
     <div id="photo">
       <PhotoItem photo={photo} />
+      <LikeContainer photo={photo} user={user} handleLike={handleLike} />
     </div>
   );
 };
